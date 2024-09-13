308d3661368ad23ec9fcdaf74ba1dc26a5b3da38
ydctnr <152324574+ydctnr@users.noreply.github.com>
2024-09-13 22:55:20 +0300

    changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,16 @@ import closeMenu from '../assets/shared/icon-close.svg';
 const Nav = () => {
   const Links = [
     { number: "00", name: "HOME", link: "/" },
-    { number: "01", name: "DESTINATION", link: "/Destination" },
-    { number: "02", name: "CREW", link: "/Crew" },
-    { number: "03", name: "TECHNOLOGY", link: "/Technology" },
+    { number: "01", name: "DESTINATION", link: "/destination" },
+    { number: "02", name: "CREW", link: "/crew" },
+    { number: "03", name: "TECHNOLOGY", link: "/technology" },
   ];
 
   const [open, setOpen] = useState(false);
   const location = useLocation();
   
+  const closeMenuOnClick = () => setOpen(false);
+
   return (
     <div className='w-full fixed top-0 left-0 right-0'>
       <div className='md:flex items-center justify-between py-10 md:pl-10 pl-7 pr-10 md:pr-0'>
@@ -27,6 +29,7 @@ const Nav = () => {
             onClick={() => setOpen(!open)} 
             className='text-3xl cursor-pointer md:hidden'
             aria-label={open ? 'Close menu' : 'Open menu'}
+            aria-expanded={open}
           >
             <img src={open ? closeMenu : hamburgerMenu} alt={open ? 'Close menu' : 'Open menu'} />
           </div>
@@ -35,18 +38,21 @@ const Nav = () => {
         <div className='lg:absolute lg:right-[35rem] lg:z-50 lg:w-[36%] lg:h-[0.031em] md:bg-gray-500'></div>
 
         <ul 
-           className={`md:flex md:items-center pt-5 pl-7 pr-24 absolute md:static bg-navbar opacity-[0.9] backdrop-blur-lg md:z-auto z-[-1] md:w-auto transition-all duration-500 ease-in-out transform ${
-            open ? 'top-20 opacity-100' : 'top-[-490px] opacity-0'
+           className={`absolute m-auto right-0 md:m-0 md:flex items-center mt-5 md:mt-0 pt-5 pb-1 md:pb-0 pl-7 pr-24 md:static bg-gray-900 md:bg-navbar md:opacity-[0.9] md:backdrop-blur-lg md:z-auto z-[-1] md:w-auto transition-all duration-500 ease-in-out transform ${
+            open ? 'top-20 opacity-[4]' : 'top-[-490px] opacity-0'
           }`}
         >
           {Links.map((link) => (
             <li 
               key={link.name} 
-              className={`md:ml-8 text-xs font-roboto font-light tracking-wider md:my-0 my-7 border-b-2 ${location.pathname === link.link ? 'border-white' : 'border-transparent'}`}
+              className={`md:ml-8 text-xs font-roboto font-light tracking-wider md:my-0 my-7 border-b-2 ${
+                location.pathname === link.link ? 'border-white' : 'border-transparent'
+              }`}
+              onClick={closeMenuOnClick}
             >
               <Link 
                 to={link.link} 
-                className='flex items-center pb-5 text-gray-100 hover:text-gray-400 duration-500'
+                className='flex items-center pb-2 md:pb-5 text-gray-100 hover:text-gray-400 duration-500'
               >
                 <span className='hidden lg:block lg:mr-2 lg:font-extrabold lg:text-white'>{link.number}</span>
                 {link.name}
