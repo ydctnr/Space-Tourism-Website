1fa7b6f5ed7ca3206b905165324de9d60f5db567
ydctnr <152324574+ydctnr@users.noreply.github.com>
2024-09-13 23:30:50 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
@@ -18,6 +18,30 @@ const App = () => {
         <Route path="/destination" element={<Destination />} />
         <Route path="/crew" element={<Crew />} />
         <Route path="/technology" element={<Technology />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;*/
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import Destination from './components/Destination';
+import Crew from './components/Crew';
+import Technology from './components/Technology';
+
+const App = () => {
+  return (
+    <div className="overflow-hidden">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/technology" element={<Technology />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -8,6 +8,19 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);*/
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router basename="/Space-Tourism-Website">
       <App />
     </Router>
   </React.StrictMode>
